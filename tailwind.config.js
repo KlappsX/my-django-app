18671871bacbe9a4a40658f031d7a4d1390dff21
KlappsX <cezar.dumi@yahoo.com>
2024-06-21 01:08:54 +0300

    Imported Tailwind again, fixed issues on tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
-    './MyApp/templates/**/*.html',
-    './MyApp/static/src/**/*.js',
+    'MyApp/templates/**/*.html',
+    'MyApp/templates/**/*.html',
+    'MyApp/static/src/**/*.js'
   ],
   theme: {
     extend: {},
